221a3397c3251feed80b4df55e8b44dce7676590
fkhjoy <khjoyfoysal@gmail.com>
2022-08-15 15:54:40 +0600

    server side binary built

---
+++ greet/server/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/fkhjoy/gRPC-Go/greet/proto"
+	"google.golang.org/grpc"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		log.Fatalf("Couldn't listen to %v\n", addr)
+	}
+
+	log.Printf("Listening to %v\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+
+}
