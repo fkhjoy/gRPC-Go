fab5ce75fe827ce40fcf1a6778ed3e678f7fd685
fkhjoy <khjoyfoysal@gmail.com>
2022-10-28 00:58:48 +0600

    feat: added server streaming

---
+++ greet/server/long_greet.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	pb "github.com/fkhjoy/gRPC-Go/greet/proto"
+)
+
+func (s *Server) LongGreet(stream pb.GreetService_LongGreetServer) error {
+	log.Println("LongGreet function was invoked")
+
+	res := ""
+	for {
+		req, err := stream.Recv();
+		
+		if err == io.EOF {
+			stream.SendAndClose(&pb.GreetResponse{
+				Result: res,
+			})
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading client streatm: %v\n", err)
+		}
+
+		res += req.FirstName;
+	}
+
+}
